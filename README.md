93d4adedd8157520a0d1b56b6f038242e88d9b5e
luni14 <te14.niklas.lundstrom@umea.nti.se>
2016-09-09 09:29:12 +0200

    Uppdaterade Readme.md

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 #Ett simplare tre i rad spel
 
 #Logg
-* 
+* Skapade filen
+*
 
 #TODO 
 * Ska använda mig av Int
--- src/main.java
+++ src/main.java
@@ -2,8 +2,9 @@
  * Created by luni14 on 2016-09-09.
  */
 public class main {
+    int = 1A
     public static void main(String[]args){
-        
+
         System.out.println();
     }
 }
